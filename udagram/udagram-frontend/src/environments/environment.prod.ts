a7d0de8d055fd5d83e4410db02ef30349a850b29
Abdelrahman Ali <abdelrahmanalisalem@gmail.com>
2023-02-01 22:19:26 +0200

    env

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: `http://udagram-backend-dev.eba-rfaazq94.us-east-1.elasticbeanstalk.com/api/v0`,
+  apiHost: `udagram-backend-dev.eba-rfaazq94.us-east-1.elasticbeanstalk.com/api/v0`,
 };
 
 /*
